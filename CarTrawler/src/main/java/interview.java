721eba118f35068900b6eddcc54aa6fadb907376
Rahul Rajan <34196469+rahulrajanpk@users.noreply.github.com>
2021-10-19 16:59:05 +0100

    Final update

--- CarTrawler/src/main/java/interview.java
+++ CarTrawler/src/main/java/interview.java
@@ -8,12 +8,14 @@ public class interview {
 
 
     public static void main(String[] args) {
-        Response response = RestAssured.get("http://localhost:3000/test");
-        JsonPath jsonPath = new JsonPath(response.asString());
+        /* Sample way reponse  was fetched */
+       // Response response = RestAssured.get("http://localhost:3000/test");
+      //   JsonPath jsonPath = new JsonPath(response.asString());
 
-        UtilLib.getCheapestCarList(response);
+        //Sample wasy to call the function. Please READ.MD to know about the paramter
+
         UtilLib.getVehiclesByType(response, "Manual");
-        UtilLib.sortByPriceInGroup(response);
+
 
     }
 }
