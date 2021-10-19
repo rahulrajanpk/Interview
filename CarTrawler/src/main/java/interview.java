766860f81c224b46fac6b4b2ef85e6c4001be4d1
Rahul Rajan <34196469+rahulrajanpk@users.noreply.github.com>
2021-10-19 17:09:14 +0100

    Comment update

--- CarTrawler/src/main/java/interview.java
+++ CarTrawler/src/main/java/interview.java
@@ -1,22 +1,29 @@
 
 import Library.UtilLib;
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
 import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 
 public class interview {
 
 
     public static void main(String[] args) {
-        /* Sample way reponse  was fetched */
+        /* Sample way response  was fetched */
        // Response response = RestAssured.get("http://localhost:3000/test");
       //   JsonPath jsonPath = new JsonPath(response.asString());
 
-        //Sample wasy to call the function. Please READ.MD to know about the paramter
-
-        UtilLib.getVehiclesByType(response, "Manual");
-
-
+        /*---------------------------------------------------------------------------------*/
+        //Sample way to call the function. Please READ.MD to know more about the parameters
+        /*--------------------------------------------------------------------------------*/
+        //UtilLib.getCheapestCarList(response);
+        //UtilLib.getVehiclesByType(response, "Manual");
+        //UtilLib.sortByCorporate(response, Arrays.asList("FIREFLY","HERTZ","SIXT","AVIS","BUDGET","ENTERPRISE","THRIFTY"));
+        //UtilLib.sortByPriceInGroup(response);
     }
 }
 
